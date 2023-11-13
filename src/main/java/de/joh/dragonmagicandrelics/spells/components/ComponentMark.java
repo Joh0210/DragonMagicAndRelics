5ce8f7b78f9c88777f15fe29190e824e1f963c8e
Joh0210 <joh.freund02@gmail.com>
2023-11-13 20:01:26 +0100

    2.1.29 ComponentMark supports PlayerCharms

--- src/main/java/de/joh/dragonmagicandrelics/spells/components/ComponentMark.java
+++ src/main/java/de/joh/dragonmagicandrelics/spells/components/ComponentMark.java
@@ -3,7 +3,6 @@ package de.joh.dragonmagicandrelics.spells.components;
 import com.mna.api.affinity.Affinity;
 import com.mna.api.spells.ComponentApplicationResult;
 import com.mna.api.spells.SpellPartTags;
-import com.mna.api.spells.attributes.AttributeValuePair;
 import com.mna.api.spells.base.IModifiedSpellPart;
 import com.mna.api.spells.parts.SpellEffect;
 import com.mna.api.spells.targeting.SpellContext;
@@ -15,24 +14,42 @@ import de.joh.dragonmagicandrelics.capabilities.dragonmagic.PlayerDragonMagicPro
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
 /**
  * This spell writes the target's location in the Rune of Marking the caster is holding.
+ * <br>If it hits a player, and the caster has a player Charm in ints offhand, it will select this player
  * @author Joh0210
  */
 public class ComponentMark extends SpellEffect {
     public ComponentMark(final ResourceLocation registryName, final ResourceLocation guiIcon) {
-        super(registryName, guiIcon, new AttributeValuePair[0]);
+        super(registryName, guiIcon);
     }
 
     public ComponentApplicationResult ApplyEffect(SpellSource source, SpellTarget target, IModifiedSpellPart<SpellEffect> modificationData, SpellContext context) {
+        if(source.getCaster() == null){
+            return ComponentApplicationResult.FAIL;
+        }
+
         ItemStack markingRune = source.getCaster().getMainHandItem().getItem() != ItemInit.RUNE_MARKING.get() && source.getCaster().getMainHandItem().getItem() != ItemInit.BOOK_MARKS.get() ? source.getCaster().getOffhandItem() : source.getCaster().getMainHandItem();
-        if (markingRune.getItem() != ItemInit.RUNE_MARKING.get() && markingRune.getItem() != ItemInit.BOOK_MARKS.get()) {
+
+        if (markingRune.getItem() == ItemInit.PLAYER_CHARM.get()) {
+            if (target.getLivingEntity() instanceof Player playerTarget) {
+                ItemInit.PLAYER_CHARM.get().SetPlayerTarget(playerTarget, markingRune);
+            }
+            else {
+                return ComponentApplicationResult.FAIL;
+            }
+        }
+        else if (markingRune.getItem() != ItemInit.RUNE_MARKING.get() && markingRune.getItem() != ItemInit.BOOK_MARKS.get()) {
             //Player specific mark
-            if (source.isPlayerCaster()) {
+            if (source.getPlayer() != null) {
                 source.getPlayer().getCapability(PlayerDragonMagicProvider.PLAYER_DRAGON_MAGIC).ifPresent(magic -> magic.mark(target.getBlock(), target.getBlockFace(null), context.getWorld()));
+            }
+            else {
+                return ComponentApplicationResult.FAIL;
             }
         }
         else{
@@ -83,6 +100,7 @@ public class ComponentMark extends SpellEffect {
         }
     }
 
+    @Override
     public boolean canBeChanneled() {
         return false;
     }
